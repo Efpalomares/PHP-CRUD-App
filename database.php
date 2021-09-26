ea42d258fd7b1bacc4fa510ffcc19aa9ac87daa6
Edisson Palomares <palomaredif@gmail.com>
2021-09-26 11:49:12 -0500

    heroku uploading

--- database.php
+++ database.php
@@ -1,5 +1,6 @@
 <?php
-$db_conn = pg_connect("host=localhost port=5432 dbname=users_contacts user=gdev password=' '");
+//$db_conn = pg_connect("host=localhost port=5432 dbname=users_contacts user=gdev password=' '");
+$db_conn = pg_connect(getenv("DATABASE_URL"));
 
 if(!$db_conn)
 {
